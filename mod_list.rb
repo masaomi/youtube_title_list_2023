d91fc65268825ec80f4073a66f894d0f85270d8f
masaomi <masaomi.hatakeyama@gmail.com>
2023-03-25 16:49:16 +0100

    mod_title.rb

--- mod_list.rb
+++ mod_list.rb
@@ -1,19 +1,25 @@
 
+chatgpt_question = eval(ARGV[0].to_s) or true
+
 input = File.read("youtube_univ_title_list_20230325.txt")
 count = input.split('<br/>').length
 puts "合計動画タイトル数: #{count}"
-puts
-puts "="*20 + " 以下chatGPTに入力 " + "="*20
-puts
+if chatgpt_question
+  puts
+  puts "="*20 + " 以下chatGPTに入力 " + "="*20
+  puts
+end
 
 input.split('<br/>').each_with_index do |line, i|
-  if i%40==0 and i==0
-    puts "以下は動画の、タイトル、年代、視聴回数、のリストです。覚えてください。今回は返答はいりませんが覚えた動画タイトル数だけ教えてください。"
-    puts
-  elsif i%40==0
-    puts
-    puts "以下同様に動画の、タイトル、年代、視聴回数、のリストです。覚えてください。今回も返答はいりませんが覚えた合計動画タイトル数だけ教えてください。"
-    puts
+  if chatgpt_question
+    if i%40==0 and i==0
+      puts "以下は動画の、タイトル、年代、視聴回数、のリストです。覚えてください。今回は返答はいりませんが覚えた動画タイトル数だけ教えてください。"
+      puts
+    elsif i%40==0
+      puts
+      puts "以下同様に動画の、タイトル、年代、視聴回数、のリストです。覚えてください。今回も返答はいりませんが覚えた合計動画タイトル数だけ教えてください。"
+      puts
+    end
   end
   row = []
   title = line.split(/作成者/).first.gsub(/】/, '').gsub(/【/, '').strip
@@ -25,7 +31,9 @@ input.split('<br/>').each_with_index do |line, i|
   puts row.join("\t")
 end
 
-puts
-puts "今まで覚えた全ての動画タイトル数はいくつですか？"
-puts
-puts "さらに、今まで覚えた動画タイトルから動画内容を類推して動画を分ける10分野（テーマ）を簡単なわかりやすい言葉でリストしてください。さらに、合計視聴回数が多い順に分野リストを並べて、各分野の総動画タイトル数と合計視聴回数もつけてリストしてください。"
+if chatgpt_question
+  puts
+  puts "今まで覚えた全ての動画タイトル数はいくつですか？"
+  puts
+  puts "さらに、今まで覚えた動画タイトルから動画内容を類推して動画を分ける10分野（テーマ）を簡単なわかりやすい言葉でリストしてください。さらに、合計視聴回数が多い順に分野リストを並べて、各分野の総動画タイトル数と合計視聴回数もつけてリストしてください。"
+end
